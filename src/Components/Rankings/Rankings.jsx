e631508a849f17628fa9c24da17dd0f3817dc418
abu said <mdabusaidriyaz@gmail.com>
2021-09-21 02:44:32 +0600

    dekhi

--- src/Components/Rankings/Rankings.jsx
+++ src/Components/Rankings/Rankings.jsx
@@ -1,48 +1,28 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import News from '../news/News';
-import './Rankings.css'
+import './Rankings.css';
+
 
 const Rankings = () => {
     const [newses, setNewses] = useState()
     useEffect(() => {
-        fetch('https://api.thenewsapi.com/v1/news/all?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
+        fetch('https://api.currentsapi.services/v1/latest-news?%27%20+%20%27language=us&apiKey=2C--5HQdFDhX5yGc0ATnnqX2BsTXYAhc2AA2fRHuiC8x44f1')
         .then(res => res.json())
-        .then(getdata => setNewses(getdata.data))
-        .catch((err) => {
-            console.log(err);
-          });
+        .then(data => setNewses(data.news))
     },[])
-    const [newses2, setNewses2] = useState()
-    useEffect(() => {
-        fetch('https://api.thenewsapi.com/v1/news/top?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
-        .then(res => res.json())
-        .then(getdata => setNewses2(getdata.data))
-        .catch((err) => {
-            console.log(err);
-          });
-    },[])
-    const [newses3, setNewses3] = useState()
-    useEffect(() => {
-        fetch('https://api.thenewsapi.com/v1/news/similar/cc11e3ab-ced0-4a42-9146-e426505e2e67?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
-        .then(res => res.json())
-        .then(getdata => setNewses3(getdata.data))
-        .catch((err) => {
-            console.log(err);
-          });
-    },[])
-    // console.log(newses)
+    console.log(newses)
     return (
         <div className="all-news">
                 {
-                newses?.map(news => <News data={news} key={news.url}></News>)
+                newses?.map(news => <News data={news} key={news.id}></News>)
                 } 
-                {
+                {/* {
                 newses2?.map(news => <News data={news} key={news.url}></News>)
                 } 
                 {
                 newses3?.map(news => <News data={news} key={news.url}></News>)
-                } 
+                }  */}
         </div>
     );
 };

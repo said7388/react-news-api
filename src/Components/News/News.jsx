1419640ac08e7215d03b612104ef0fa0196b5c04
abu said <mdabusaidriyaz@gmail.com>
2021-09-21 00:54:57 +0600

    dekhi

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -3,13 +3,13 @@ import './News.css';
 
 const News = (pops) => {
     // const news = pops.data;
-    const {publishedAt, urlToImage, source, title, description, url} = pops.data;
-    const date = (publishedAt).substring(0, 10)
+    const {published_at, image_url, source, title, description, url} = pops.data;
+    const date = (published_at).substring(0, 10)
     // console.log(news);
     return (
         <div className="news">
-            <img className="thumbnail" src={urlToImage} alt="" />
-            <p className="source"> <small className="source-name">{source.name}</small> <small>{date}</small> </p>
+            <img className="thumbnail" src={image_url} alt="" />
+            <p className="source"> <small className="source-name">{source}</small> <small>{date}</small> </p>
             <h3>{title}</h3>
             <p>{description}</p>
             <a className="read-more" target="_blank" href={url}>Read More</a>
--- src/Components/Rankings/Rankings.jsx
+++ src/Components/Rankings/Rankings.jsx
@@ -4,20 +4,44 @@ import News from '../news/News';
 import './Rankings.css'
 
 const Rankings = () => {
-    const [newNews, setState] = useState([])
+    const [newses, setNewses] = useState()
     useEffect(() => {
-        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=faf990aa96f8410aadb1faf9d951d1ef')
+        fetch('https://api.thenewsapi.com/v1/news/all?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
         .then(res => res.json())
-        .then(data => setState(data.articles))
+        .then(getdata => setNewses(getdata.data))
         .catch((err) => {
             console.log(err);
           });
     },[])
-    // console.log(newNews)
+    const [newses2, setNewses2] = useState()
+    useEffect(() => {
+        fetch('https://api.thenewsapi.com/v1/news/top?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
+        .then(res => res.json())
+        .then(getdata => setNewses2(getdata.data))
+        .catch((err) => {
+            console.log(err);
+          });
+    },[])
+    const [newses3, setNewses3] = useState()
+    useEffect(() => {
+        fetch('https://api.thenewsapi.com/v1/news/similar/cc11e3ab-ced0-4a42-9146-e426505e2e67?api_token=KOV1PRMNZxOakzJFbwZp5F2V12aMh1dknhXBWy1F')
+        .then(res => res.json())
+        .then(getdata => setNewses3(getdata.data))
+        .catch((err) => {
+            console.log(err);
+          });
+    },[])
+    // console.log(newses)
     return (
         <div className="all-news">
                 {
-                newNews?.map(news => <News data={news} key={news.url}></News>)
+                newses?.map(news => <News data={news} key={news.url}></News>)
+                } 
+                {
+                newses2?.map(news => <News data={news} key={news.url}></News>)
+                } 
+                {
+                newses3?.map(news => <News data={news} key={news.url}></News>)
                 } 
         </div>
     );
